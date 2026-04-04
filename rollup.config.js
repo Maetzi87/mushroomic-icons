0dc17d46a967ff7372d0fe62aaccdee77491e07f
Maetzi87 <118321707+Maetzi87@users.noreply.github.com>
2026-04-05 01:16:15 +0200

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,9 @@ import fs from "fs";
 import path from "path";
 import typescript from "@rollup/plugin-typescript";
 import json from "@rollup/plugin-json";
-import nodeResolve from "@rollup/plugin-node-resolve";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import terser from "@rollup/plugin-terser";
+import { terser } from "@rollup/plugin-terser";
 
 function generateIconList() {
   return {
