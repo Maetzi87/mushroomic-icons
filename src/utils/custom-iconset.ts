75f83274f79df8f22517d8d6e3c541a5a06048c4
Maetzi87 <eva.stoiser@gmx.net>
2026-04-05 19:40:05 +0200

    Use structure of bha to make it work

--- src/utils/custom-iconset.ts
+++
@@ -1,4 +0,0 @@
-export const customIconset = (name: string, iconset: any) => {
-  (window as any).customIconsets = (window as any).customIconsets || {};
-  (window as any).customIconsets[name] = iconset;
-};
